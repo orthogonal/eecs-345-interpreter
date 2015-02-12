017322af8bb8a57e397052e4c325ea1745274041
Jon Pfeil <piffle21@gmail.com>
2015-02-12 18:02:00 -0500

    Rewrote tests to use rackunit

--- test.scm
+++ test.scm
@@ -1,24 +1,14 @@
 ;Logic necessary to run through test suite
+(require rackunit)
 (load "interpret.scm")
 
-(define eq_test
-    (lambda (test_n val)
-        (display test_n)
-        (display ": ")
-        (display (= (interpret test_n) val))
-        (display "\n")
-    )
-)
-
-(eq_test "tests/1" 150)
-(eq_test "tests/2" -4)
-(eq_test "tests/3" 10)
-(eq_test "tests/4" 16)
-(eq_test "tests/5" 220)
-(eq_test "tests/6" 5)
-(eq_test "tests/7" 6)
-(eq_test "tests/8" 10)
-(eq_test "tests/9" 5)
-(eq_test "tests/10" -39)
-
-#t
+(check-eq? (interpret "tests/1") 150)
+(check-eq? (interpret "tests/2") -4)
+(check-eq? (interpret "tests/3") 10)
+(check-eq? (interpret "tests/4") 16)
+(check-eq? (interpret "tests/5") 220)
+(check-eq? (interpret "tests/6") 5)
+(check-eq? (interpret "tests/7") 6)
+(check-eq? (interpret "tests/8") 10)
+(check-eq? (interpret "tests/9") 5)
+(check-eq? (interpret "tests/10") -39) 
